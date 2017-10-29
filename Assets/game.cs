bcb0c52b814297e196c560b3c501c5e3e5046e6a
駒井遥 <mariogalaxy3kibou@yahoo.co.jp>
2017-10-29 16:37:01 +0900

    battle

---
+++ Assets/game.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			SceneManager.LoadScene("battle");
+		}
+	}
+}
---
+++ Assets/throws.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throws : MonoBehaviour {
+	GameObject ball;
+	public Camera camera;
+	public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.K)) {
+			Shot();
+			Debug.Log("打った");
+		}
+		
+	}
+
+	void Shot(){
+		int distance = 100;
+		Vector3 center = new Vector3 (Screen.width / 2, Screen.height / 2, 0);
+		Ray ray = camera.ScreenPointToRay (center);
+		RaycastHit hitInfo;
+		if (Physics.Raycast (ray, out hitInfo, distance)) {
+			Debug.Log ("あたった");
+			if (hitInfo.collider.tag == "enemy") {
+				Destroy(hitInfo.collider.gameObject);
+				Debug.Log ("消した");
+			}
+		}
+	}
+}
